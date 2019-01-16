305b050c7bee97b5d72dbabfe2d60d77f817e2fa
frh0792 <frh0792@163.com>
2019-01-16 09:33:20 +0800

    初始化提交，从原项目剥离扩展插件

---
+++ Frh.K3.FastExtension/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frh.K3.FastExtension
+{
+    public class Class1
+    {
+    }
+}
